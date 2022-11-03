156679ba7b5cde4d2b8f06536617cccb9c55584e
Kentaro-Machida <kenkenm120319@gmail.com>
2022-11-03 21:51:32 +0900

    対象パスを変更しやすくした

--- preprocess/Line2Polygon2VOC.bash
+++ preprocess/Line2Polygon2VOC.bash
@@ -1,2 +1,6 @@
-python line2polygon.py --input_dir ../processed_data/labelme_annotations/test_datas --output_dir ../processed_data/polygons/test_datas_polygon
-python ./labelme2voc/labelme2voc.py ../processed_data/polygons/test_datas_polygon ../processed_data/voc_datasets/test_dataset_voc --labels ./labelme2voc/labels.txt
+LINESTRIP_DIR="../processed_data/labelme_annotations/956_datas"
+POLYGON_DIR="../processed_data/polygons/956_datas_polygon2"
+VOC_DIR="../processed_data/voc_datasets/956_dataset2_voc"
+LABEL_TEXT="./labelme2voc/labels.txt"
+python line2polygon.py --input_dir $LINESTRIP_DIR --output_dir $POLYGON_DIR
+python ./labelme2voc/labelme2voc.py $POLYGON_DIR $VOC_DIR --labels $LABEL_TEXT
